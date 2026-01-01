709b2ed8226922b4ab86f136a860a42bac360b21
ComplexRobot <complexrobotx@gmail.com>
2025-12-31 19:47:16 -0800

    Initial creation of project with base Avalonia UI code.

---
+++ README.md
@@ -0,0 +1,4 @@
+## SurrealEdit
+
+ > [!WARNING]
+ > WIP
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SurrealEdit.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
