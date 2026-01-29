16c94b1d05a5fd0205ffe46b5eabf975c2a00e82
ComplexRobot <complexrobotx@gmail.com>
2026-01-28 22:50:19 -0800

    Clean up and add `OrderBy` to sort elements by their name.

--- SurrealEdit/Nodes/Validators/EnumValidator.cs
+++ SurrealEdit/Nodes/Validators/EnumValidator.cs
@@ -11,6 +11,10 @@ namespace SurrealEdit.Nodes.Validators;
 public class EnumValidator<T> : SetValidator<T> where T : Enum {
 	/// <inheritdoc/>
 	public override ICollection<T?> ValidValues { get; set; } =
-		(HashSet<T?>)
-		[..typeof(T).GetFields(BindingFlags.Public | BindingFlags.Static).Select(x => (T?)x.GetValue(null))];
+		(HashSet<T?>)[
+			..typeof(T)
+			.GetFields(BindingFlags.Public | BindingFlags.Static)
+			.Select(fieldInfo => (T?)fieldInfo.GetValue(null))
+			.OrderBy(value => $"{value}")
+		];
 }
